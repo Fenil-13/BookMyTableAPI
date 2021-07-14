5dfc9ce89f1aa8622ba441d36f7e514e20bc80fb
Fenil Moradiya <fjmoradiya@gmail.com>
2021-07-14 21:56:05 +0530

    Sign up + update user profile Api

--- __init__.py
+++ __init__.py
@@ -1,59 +1,123 @@
-from flask import Flask, render_template, request ,session,jsonify
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 import os
 import json
+from bson.objectid import ObjectId
 from werkzeug.utils import secure_filename
 import certifi
 
-
-with open('config.json', 'r+') as c:
+with open("config.json", "r+") as c:
     params = json.load(c)["params"]
 
-if params['local_server'] == "True":
+if params["local_server"] == "True":
     local_server = True
 else:
     local_server = False
 
 app = Flask(__name__)
-app.config['upload_location'] = params['upload_location']
-client = MongoClient("mongodb+srv://admin:krupal123@cluster0.r0oii.mongodb.net/BookMyTable?retryWrites=true&w=majority",tls=True,tlsCAFile=certifi.where())
+if local_server:
+    app.config["upload_location"] = params["local_upload_location"]
+else:
+    app.config["upload_location"] = params["prod_upload_location"]
+
+client = MongoClient(params["database_url"], tls=True, tlsCAFile=certifi.where())
 db = client.BookMyTable
 userCollection = db.Users
 
-# res = userCollection.find_one({'_id': ObjectId('60eee32180b8955038913e5f')})
-all_user = userCollection.find()
 
-@app.route("/signupuser",methods=['GET','POST'])
+@app.route("/sign_up_user", methods=["GET", "POST"])
 def signupUser():
-    result=dict();
-    if request.method=="POST":
-        user=userCollection.find({"user_phone_number":request.form["user_phone_number"]})
-        if (user==None) :
-            result["success"]=1;
-            result["user_status"]="available"
-        else :
+    result = dict()
+    result["success"] = 0
+    if request.method == "POST":
+        # Check User exits or Not By Phone Number
+        user = userCollection.find(
+            {"user_phone_number": request.form["user_phone_number"]}
+        )
+        if user is None:
+            result["success"] = 1
+            result["user_status"] = "available"
+        else:
+            # Save Profile Pic in folder and save file name in Db 
+            profile_pic = request.files["user_profile_pic"]
+            profile_pic_file_name = secure_filename(
+                request.form["user_auth_id"] + "_profile_pic_" + profile_pic.filename
+            )
+            profile_pic.save(
+                os.path.join(app.config["upload_location"], profile_pic_file_name)
+            )
 
-            profile_pic = request.files['user_profile_pic']
-            profile_pic.save(os.path.join(
-                    app.config['upload_location'], secure_filename(profile_pic.filename)))
+            user = {
+                "user_auth_id": request.form["user_auth_id"],
+                "user_name": request.form["user_name"],
+                "user_phone_number": request.form["user_phone_number"],
+                "user_email": request.form["user_email"],
+                "user_password": request.form["user_password"],
+                "user_profile_pic": profile_pic_file_name,
+                "user_device_token": request.form["user_device_token"],
+                "user_location": request.form["user_location"],
+            }
 
-            user={"user_auth_id":request.form["user_auth_id"],
-            "user_name":request.form["user_name"],
-            "user_phone_number":request.form["user_phone_number"],
-            "user_email":request.form["user_email"],
-            "user_password":request.form["user_password"],
-            "user_profile_pic": profile_pic.filename,
-            "user_device_token":request.form["user_device_token"],
-            "user_location":request.form["user_location"]
-                }
-            
             userCollection.insert_one(user)
-            result["success"]=1;
-            result["user_status"]="user_created"
-    else:
-         result["success"]=0;
 
+            result["success"] = 1
+            result["user_status"] = "user_created"
+
+    return jsonify(result)
+
+
+@app.route("/update_user", methods=["GET"])
+def updateUser():
+    result = dict()
+    result["success"] = 0
+
+    user=None
+    
+    try:
+        user = userCollection.find_one({"_id": ObjectId(request.form["user_id"])})
+    except:
+        result["success"] = 1
+        result["status"] = "user_not available"
+
+    if user is None:
+        result["success"] = 1
+        result["status"] = "user_not available"
+    else:
+        profile_pic_file_name = user["user_profile_pic"]
+
+        # if any Change in Profile Pic then upload and change file name
+       
+        if request.form["profile_pic_updated"] == "true":
+
+            #First Remove exits file so server keep clean
+            os.remove(app.config["upload_location"]+user["user_profile_pic"])
+
+            #Now Upload New File
+            profile_pic = request.files["user_profile_pic"]
+            profile_pic_file_name = secure_filename(
+                request.form["user_auth_id"] + "_profile_pic_" + profile_pic.filename
+            )
+            profile_pic.save(
+                os.path.join(app.config["upload_location"], profile_pic_file_name)
+            )
+
+        old_user = {"_id": ObjectId(request.form["user_id"])}
+        updated_user = {
+            "$set": {
+                "user_auth_id": request.form["user_auth_id"],
+                "user_name": request.form["user_name"],
+                "user_phone_number": request.form["user_phone_number"],
+                "user_email": request.form["user_email"],
+                "user_password": request.form["user_password"],
+                "user_profile_pic": profile_pic_file_name,
+                "user_device_token": request.form["user_device_token"],
+                "user_location": request.form["user_location"],
+            }
+        }
+
+        userCollection.update_one(old_user, updated_user)
+        result["success"] = 1
+        result["status"] = "user_updated_successfully"
     return jsonify(result)
 
 
